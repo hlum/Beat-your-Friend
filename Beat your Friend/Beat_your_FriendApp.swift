1156730a3287d31b748494664d23b02815371c1c
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-11 14:34:41 +0900

    Initial Commit

---
+++ Beat your Friend/Beat_your_FriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beat_your_FriendApp.swift
+//  Beat your Friend
+//
+//  Created by cmStudent on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Beat_your_FriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
